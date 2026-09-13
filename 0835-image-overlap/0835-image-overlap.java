0104a57feece19cd085fca3d00e31a4fb61069bc
Siddhartha Karre <karresiddhartha56@gmail.com>
2026-09-13 19:13:49 +0530

    Time: 57 ms (56.82%), Space: 44.3 MB (89.02%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.java
+++ 0835-image-overlap/0835-image-overlap.java
@@ -1,25 +1,20 @@
 class Solution {
     public int largestOverlap(int[][] img1, int[][] img2) {
         int n=img1.length;
-        HashMap<String, Integer> freq = new HashMap<>();
         int max=0;
-        List<int[]> ones1= new ArrayList<>();
-        List<int[]> ones2= new ArrayList<>();
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(img1[i][j]==1) ones1.add(new int[]{i,j});
-                if(img2[i][j]==1) ones2.add(new int[]{i,j});
+        for(int r=-n+1; r<n; r++){
+            for(int c=-n+1; c<n; c++){
+                int overlap=0;
+                for(int i=0; i<n; i++){
+                    for(int j=0; j<n; j++){
+                        int x=i+r, y=j+c;
+                        if(x>=0 && x<n && y>=0 && y<n && img1[i][j] ==1 && img2[x][y]==1)
+                        overlap++;
+                    }
+                }
+                max=Math.max(overlap, max);
             }
         }
-        for(int[] a: ones1){
-            for(int[] b: ones2){
-                String key = (b[0]-a[0])+","+(b[1]-a[1]);
-                int count = freq.getOrDefault(key, 0)+1;
-                freq.put(key, count);
-                max=Math.max(count, max);
-            }
-        }
-        
         return max;
     }
 }
