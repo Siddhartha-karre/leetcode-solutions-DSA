db7a79c9dce784b1e4afd6eb17d49efe6bd0ab09
Siddhartha Karre <karresiddhartha56@gmail.com>
2026-09-16 21:39:15 +0530

    Time: 168 ms (11.61%), Space: 193.2 MB (9.59%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -1,20 +1,23 @@
 class Solution {
-    private boolean isPalindrome(int i, int j, String s){
-        while(i<=j){
-            if(s.charAt(i)!=s.charAt(j)) return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
     public int maxPalindromes(String s, int k) {
         int n = s.length();
         if (k == 1)
             return n;
         int[][] dp = new int[n + 1][n + 1];
+        boolean[][] isPalindrome = new boolean[n][n];
+        for(int l=1; l<=n; l++){
+            for(int i=0; i+l<=n; i++){
+                int j=i+l-1;
+                if(i==j) isPalindrome[i][j]=true;
+                else if(i+1==j) isPalindrome[i][j]=(s.charAt(i)==s.charAt(j));
+                else{
+                    isPalindrome[i][j]=(s.charAt(i)==s.charAt(j)) && isPalindrome[i+1][j-1];
+                }
+            }
+        }
         for (int i = n - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
-                if (isPalindrome(i, j, s)) {
+                if (isPalindrome[i][j]) {
                     int take = 1 + ((j+k<=n)?dp[j + 1][j + k]:0);
                     int grow = dp[i][j + 1];
                     int slide = dp[i + 1][j + 1];
